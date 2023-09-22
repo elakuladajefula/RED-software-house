a99adcfe3eb4c625d444b5eec22c10ab90f47ba0
elakuladajefula <ela.wisniewska3@wp.pl>
2023-09-22 17:33:45 +0200

    Fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+class Weather {
+  constructor(city, date, temperature) {
+    this.city = city;
+    this.date = date;
+    this.temperature = temperature;
+  }
+}
 
 function App() {
+  const [city, searchCity] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    let data = await collectData(city);
+    if (data.length == 2) {
+      console.log(data[0]);
+      console.log(getDate());
+      console.log(data[1]);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={handleSubmit}>
+      <label>Search city:
+        <input 
+          type="text" 
+          value={city}
+          onChange={(e) => searchCity(e.target.value)}
+        />
+      </label>
+      <input type="submit" />
+    </form>
+  )
+}
+
+async function collectData(city) {
+  const apiUrl = "https://api.weatherapi.com/v1/current.json?key=3dfce0a016e541dbb87120605232209&q=";
+  const fullUrl = apiUrl + city;
+  const response = await fetch(fullUrl);
+  const returnData = [];
+  if (response.status == 200) {
+    const data = await fetch(fullUrl).then((data) => data.json());
+    const returnData = [data.location.name, data.current.temp_c];
+    return returnData;
+  } else {
+    alert("Error fetching data");
+    return returnData;
+  }
+}
+
+function getDate() {
+  let dateObj = new Date();
+  let month = dateObj.getUTCMonth() + 1;
+  let day = dateObj.getUTCDate();
+  let year = dateObj.getUTCFullYear();
+  return year + "-" + month + "-" + day;
 }
 
 export default App;
